a64609c66105216f3a3bdf966f91ce25e2dfe3c8
Natalia Smythe <nataliaasmythe@gmail.com>
2025-04-09 20:27:38 -0400

    Initial Commit

---
+++ MuseIQ/MuseIQApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuseIQApp.swift
+//  MuseIQ
+//
+//  Created by Natalia Smythe on 4/9/25.
+//
+
+import SwiftUI
+
+@main
+struct MuseIQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
